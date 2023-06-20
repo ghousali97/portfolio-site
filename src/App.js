2da20340849dc6abd70eaff65f6ba46e986674ba
ghousalik97 <ghousali11@gmail.com>
2023-06-20 23:12:56 +0800

    added responsiveness

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Topbar from './components/topbar/Topbar.jsx'
 import Contact from './components/contact/Contact.jsx'
 import Portfolio from './components/portfolio/Portfolio.jsx';
 import Testimonials from './components/testimonials/Testimonials.jsx';
-import Works from './components/works/Works.jsx';
 import Intro from './components/intro/Intro.jsx';
 import Menu from './components/menu/Menu';
 
